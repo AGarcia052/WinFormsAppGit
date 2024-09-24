3249b125aafe9c5014737564edb7564602daa7eb
AGarcia05 <agarciadelaplazaperulero@gmail.com>
2024-09-24 10:02:07 +0200

    boton aceptar

--- WinFormsApp1/WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -28,18 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btAceptar = new Button();
             SuspendLayout();
+            // 
+            // btAceptar
+            // 
+            btAceptar.Location = new Point(222, 130);
+            btAceptar.Name = "btAceptar";
+            btAceptar.Size = new Size(280, 103);
+            btAceptar.TabIndex = 0;
+            btAceptar.Text = "Aceptar";
+            btAceptar.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btAceptar);
             Name = "Form1";
             Text = "Formulario de ejemplo mod";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btAceptar;
     }
 }
